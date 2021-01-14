b4d820faf13ba3fa93c72aaffe20cdb6fadfc0bf
kiacolbert <colber16@gmail.com>
2021-01-14 08:50:35 -0500

    fix ALL merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-=======
-import Button from './components/Button';
-import StockForm from './components/StockForm';
->>>>>>> c7b3c2b2f3a0f176024051c9cb45b7858a64c8f9
 import StockList from './components/StockList';
 
 function App() {
