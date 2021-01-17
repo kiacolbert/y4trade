b8e014becc417adf87ca88a86e3f375c5ac83153
kiacolbert <colber16@gmail.com>
2021-01-17 11:24:10 -0500

    use polish from alpha library instead of custom func to clean data

--- src/components/StockForm.js
+++ src/components/StockForm.js
@@ -3,35 +3,22 @@ import Alpha from 'alphavantage';
 
 const StockForm = ({addStock}) => {
   const [stockName, setStockName] = useState('');
-  // needs error handling
   const onSubmit = (e) => {
     e.preventDefault();
     const alpha = Alpha({key: process.env.REACT_APP_ALPHAVANTAGE_KEY})
     alpha.data.intraday(stockName).then((data) => {
-      let cleaned = cleanData(data);
-      let reOrged = reorganizeTimeSeries(cleaned.timeSeries);
-      addStock({name:stockName, data:{ metaData: cleaned.metaData, timeSeries: reOrged}});
-    });
+      debugger;
+      const cleaned = alpha.util.polish(data);
+      let reOrged = reorganizeTimeSeries(cleaned.data);
+      addStock({name:stockName, data:{ metaData: cleaned.meta, timeSeries: reOrged}});
+    }).catch(error => alert(error.message ?? 'problem retrieving data. check ticker symbol.'));
     setStockName('');
   }
-  const cleanData = (data) => {
-    const META_DATA = 'Meta Data'
-    const TIME_SERIES_1_MIN = 'Time Series (1min)'
-    const newData =  {};
-    Object.keys(data[META_DATA]).forEach(oldKey => {
-      let newKey = oldKey.replace(/\d\D\s/,'');
-      newData[newKey] = data[META_DATA][oldKey];
-    });
-    const newTimeData = {};
-    Object.keys(data[TIME_SERIES_1_MIN]).forEach(time => {
-      newTimeData[time] = {};
-      Object.keys(data[TIME_SERIES_1_MIN][time]).forEach(key => {
-        let newKey = key.replace(/\d\D\s/,'');
-        newTimeData[time][newKey] = data[TIME_SERIES_1_MIN][time][key];
-      })
-    });
-    return { metaData: newData, timeSeries: newTimeData};
-  }
+  /**
+   * transposes and separates data in categories.
+   * @param {Object} timeSeries 
+   * @returns {Object}
+   */
   const reorganizeTimeSeries = (timeSeries) => {
     let date = [];
     let close = [];
